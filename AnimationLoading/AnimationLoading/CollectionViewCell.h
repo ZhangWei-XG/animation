d2207d70af117e3b37b7b51987eb737090242a8c
张维 <zhangweijzsz@163.com>
2022-12-09 15:15:57 +0800

    这是一个测试信息

---
+++ AnimationLoading/AnimationLoading/CollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.h
+//  AnimationLoading
+//
+//  Created by 张维 on 2022/12/9.
+//  Copyright © 2022 zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AnimationLoading/AnimationLoading/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  AnimationLoading
+//
+//  Created by 张维 on 2022/12/9.
+//  Copyright © 2022 zhang. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
